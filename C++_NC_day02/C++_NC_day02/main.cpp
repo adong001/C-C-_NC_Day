333bc091a8e23d4ddee3207265b939ac4313c66b
adong <1792095378@qq.com>
2019-08-26 21:02:41 +0800

    提交代码

---
+++ C++_NC_day02/C++_NC_day02/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//1、分别使用引用和宏完成交换方法（使用内联），体会两者异同
+//2、按照指定的函数声明，完成函数：
+//a、将一个字符串转换为数字，返回值用来校验转换是否成功
+//bool strtoi(const char *, int &);
+//b、将一个数字通过指定进制按位逆序（直接覆盖原数）
+//void sysReverse(int &, int);
+int main()
+{
+    system("pause");
+    return 0;
+}
