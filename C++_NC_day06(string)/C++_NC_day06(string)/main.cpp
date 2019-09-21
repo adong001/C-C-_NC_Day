e7f6794d8583e86607eda49f8c8e26e23dba4782
adong <1792095378@qq.com>
2019-09-21 12:38:32 +0800

    提交代码

--- C++_NC_day06(string)/C++_NC_day06(string)/main.cpp
+++ C++_NC_day06(string)/C++_NC_day06(string)/main.cpp
@@ -1,26 +1,14 @@
-#include"String.h"
+#include"HighAcc.h"
+
 int main()
 {
-	String s("student");
-	const String s2("studentr");
-	cout << ("student" > s);
-	cout << ("studentw" >= s);
-	cout << ("student" <= s);
-	cout << ("student" > s);
-	cout << ("student" != s);
-	/*cout << s + "kkk" << endl;
-	cout << s + s2 << endl;
-	cout << "KKKK" + s << endl;*/
-	/*cout << (s >= "student");
-	cout << (s <= "studente");
-	cout << (s > "student");
-	cout << (s >= s2);
-	cout << (s <=s2);
-	cout << (s <= s2);*/
-	/*cout << (s > "student");
-	cout << (s > "student");
-	cout << (s > "student");
-	cout << (s > "student");*/
-	system("pause");
-	return 0;
+	string s1;
+	string s2;
+	cin >> s1;
+	cin >> s2;
+	HighAcc a(s1);
+	HighAcc b(s2);
+	cout << (string)(a * b);
+    system("pause");
+    return 0;
 }
