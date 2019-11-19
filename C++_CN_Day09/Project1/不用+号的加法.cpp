3061770f1d01a2a6a43178bc8ecd80a4a8e40c30
adong <1792095378@qq.com>
2019-11-19 12:20:10 +0800

    1

--- C++_CN_Day09/Project1/不用+号的加法.cpp
+++ C++_CN_Day09/Project1/不用+号的加法.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 class UnusualAdd {
 public:
-	int addAB(int A, int B)
+	/*int addAB(int A, int B)
 	{
 		return(A - (-B));
+	}*/
+
+	int addAB(int A, int B)
+	{
+		if (B == 0)
+		{
+			return A;
+		}
+
+		int sum = 0, carry = 0;
+		{    // 1+1=0  1+0=1 0+1=1  0+0=0 这就是两个数任意一位相加的情况   
+			sum = (A^B);//1.记录两个数的异或值，它代表两个数的那一位不相同，就不用进位，0+0虽然不用进位，但和还是0，不用考虑。 
+			carry = (A&B) << 1;//记录两个数按位与的值，他代表两个数需要进位的位数，将1左移这些位
+			return addAB(sum, carry);//进位和不不进位相加递归直到进位为0
+		}
 	}
+
 };
-int main1()
+int main()
 {
 	UnusualAdd Test;
 	cout << Test.addAB(1, 9) << endl;
--- C++_CN_Day09/Project1/棋盘左上角到右下角的走法数.cpp
+++ C++_CN_Day09/Project1/棋盘左上角到右下角的走法数.cpp
@@ -1,21 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+//
+//分为以下三种情况：
+//	1.当n = 1,m = 1时 ，总路径为1 + 1 = 2种
+//	2.当n = 1(或m = 1)且 m > 1(n > 1)时，总路径为n +  m种，，，1.2情况可合并，此处只是为了更容易理解先抛出1
+//	3.当n > 1 且 m > 1时，最终到达(n,m)点的情况只有两种 (n-1,m)和(n,m-1).这样的话可以通过递归相加两种情况的和
 
-
-
-int main()
+int pathnum(int n, int m)
 {
-	int n = 2;
-	int m = 2;
-	int N = n + m;
-	int K = n;
-	double res = 1.0;
-	for (int i = 0; i < n; ++i)
+	if (n > 1 && m > 1)
 	{
-		res = res * (N - K + i) / i;
+		return pathnum(n - 1, m) + pathnum(n, m - 1);
 	}
-	cout<<(int)res<<endl;
-    system("pause");
-    return 0;
+
+	else if (n == 1 || m == 1)
+	{
+		return n + m;
+	}
+
+	else
+	{
+		return 0;
+	}
+}
+
+int main2()
+{
+	int n, m;
+	while (cin >> n >> m)
+	{
+		cout << pathnum(n, m) << endl;
+	}
+	return 0;
 }
