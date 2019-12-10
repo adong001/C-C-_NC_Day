e9321041e12ebe2bd3a70af20b3ccda9b62e9722
adong <1792095378@qq.com>
2019-12-10 14:20:40 +0800

    1

--- C++_CN_Day30/C++_CN_Day30/因子个数.cpp
+++ C++_CN_Day30/C++_CN_Day30/因子个数.cpp
@@ -11,10 +11,18 @@ int main()
 		int count = 0;
 		for (int i = 2; i <= sqrt(num); i++)
 		{
-			if (num%i == 0)
+			if (num%i == 0)//找到一个因子
 			{
-				count++;
+				while (num%i == 0)//不断的短除它，直到不能除尽为止
+				{
+					num /= i;
+				}
+				count++;//对不同因子count+1即可
 			}
+		}
+		if (num != 1)//出来以后，如果num不为1说明有一个因子是一个单个的质数，在给count加一次
+		{
+			count++;
 		}
 		cout << count << endl;
 	}
--- C++_CN_Day30/C++_CN_Day30/最难的问题.cpp
+++ C++_CN_Day30/C++_CN_Day30/最难的问题.cpp
@@ -1,9 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main1()
+int main2()
 {
+	string src;
+	while (getline(cin ,src))
+	{
+		for (auto& ch : src)
+		{
+			if ('A' <= ch && ch <= 'E')
+			{
+				ch += 21;
+			}
+			else if ('E' < ch && ch <= 'Z')
+			{
+				ch -= 5;
+			}
+			else
+			{
+				//do nothing
+			}
+		}
+		cout << src << endl;
+	}
     system("pause");
     return 0;
 }
