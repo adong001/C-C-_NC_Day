7ced6e7c8a41b1d366db498524e910a0fe37fcef
adong <1792095378@qq.com>
2020-01-09 20:16:53 +0800

    1

---
+++ C++_CN_Day33/C++_CN_Day33/仅仅翻转字母.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+//给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，
+//而所有字母的位置发生反转。
+
+//示例 1：
+//输入："ab-cd"
+//输出："dc-ba"
+
+//示例 2：
+//输入："a-bC-dEf-ghIj"
+//输出："j-Ih-gfE-dCba"
+
+//示例 3：
+//输入："Test1ng-Leet=code-Q!"
+//输出："Qedo1ct-eeLg=ntse-T!"
+
+//提示：
+//S.length <= 100
+//33 <= S[i].ASCIIcode <= 122
+//S 中不包含 \ or "
+
+class Solution {
+public:
+	string reverseOnlyLetters(string S) 
+	{
+
+	}
+};
+
+int main4()
+{
+    system("pause");
+    return 0;
+}
---
+++ C++_CN_Day33/C++_CN_Day33/有序数组的平方.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+using namespace std;
+//给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+//
+//示例 1：
+//输入：[-4, -1, 0, 3, 10]
+//输出：[0, 1, 9, 16, 100]
+//
+//示例 2：
+//输入：[-7, -3, 2, 3, 11]
+//输出：[4, 9, 9, 49, 121]
+//
+//
+//提示：
+//1 <= A.length <= 10000
+//- 10000 <= A[i] <= 10000
+//A 已按非递减顺序排序。
+
+class Solution {
+public:
+	vector<int> sortedSquares(vector<int>& A) 
+	{
+
+	}
+};
+
+int main3()
+{
+    system("pause");
+    return 0;
+}
