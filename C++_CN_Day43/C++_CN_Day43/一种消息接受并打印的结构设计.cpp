4c14dbbbc313ed7235812ed88ca3298e4bc6be45
adong <1792095378@qq.com>
2020-04-02 09:38:06 +0800

    12

--- C++_CN_Day43/C++_CN_Day43/一种消息接受并打印的结构设计.cpp
+++ C++_CN_Day43/C++_CN_Day43/一种消息接受并打印的结构设计.cpp
@@ -1,10 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
-#include<list>
-#include<priority_queue>
-#include<algorithm>
-
+#include<queue>
+#include<functional>
 //int main()
 //{
 //	int tmp;
@@ -39,10 +37,28 @@
 //
 //    return 0;
 //}
-
 int main()
 {
-	int tmp;
-	priority_queue<int, vector<int>> q;
+	int n, Min = 1, k = 1;
+	std::priority_queue<int, std::vector<int>, std::greater<int>> q;//小堆
+	while (std::cin >> n)
+	{
+		std::vector<int> v(n);
+		for (int i = 0; i < n; i++)
+		{
+			std::cin >> v[i];
+			q.push(v[i]);
+			if (v[i] == Min)//找到当前最小
+			{
+				while (!q.empty() && q.top() == Min)
+				{
+					std::cout << k++ << " " << v[i] << std::endl;
+					Min++;
+					q.pop();//将最小的以后的都pop
+				}
+			}
+		}
+
+	}
 	return 0;
 }
