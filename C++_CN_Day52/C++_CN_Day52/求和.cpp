df625999910272a2e9e1e2b646f7524dae189510
adong <1792095378@qq.com>
2020-04-12 20:41:45 +0800

    1

---
+++ C++_CN_Day52/C++_CN_Day52/求和.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+
+int GetAdd(int n, int m)
+{
+	if (m < 1)
+	{
+		return 0;
+	}
+	if (m == 1)
+	{
+		return 1;
+	}
+	for (int i = 0;i<)
+			//std::cout << i << " " << num - i << std::endl;
+	
+		//std::cout << num<<std::endl;
+}
+
+int main()
+{
+	int n, num;
+	while (std::cin >> n >> num)
+	{
+		GetAdd(n, m);
+	}
+    return 0;
+}
---
+++ C++_CN_Day52/C++_CN_Day52/电话号码.cpp
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+
+int main1()
+{
+    return 0;
+}
