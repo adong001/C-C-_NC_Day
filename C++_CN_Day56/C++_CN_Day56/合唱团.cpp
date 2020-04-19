74b24b180234e4dd427493595ce97cd8674cd8b0
adong <1792095378@qq.com>
2020-04-19 20:54:00 +0800

    123

--- C++_CN_Day56/C++_CN_Day56/合唱团.cpp
+++ C++_CN_Day56/C++_CN_Day56/合唱团.cpp
@@ -1,38 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 struct Person
 {
-	int num;
 	int weight;
 	int height;
-
-	friend std::istream & operator >> (std::istream &is, Person &p);
-	bool operator >(const Person& p)
-	{
-		return ((weight >= p.weight) && (height >= p.height));
-	}
 };
-
+bool cmp(const Person& p1, const Person& p2)
+{
+	if (p1.weight != p2.weight)
+	{
+		return p1.weight < p2.weight;//体重按从小到大排
+	}
+	else
+	{
+		return p1.height <= p2.height;//身高按从高到低
+	}
+}
 std::istream & operator >> (std::istream &is, Person &p)
 {
-	is >> p.num >> p.weight >> p.height;
+	is >> p.weight >> p.height;
 	return is;
 }
 
-int main()
+int main2()
 {
 	int N;
 	while (std::cin >> N)
 	{
 		int count = 0;
-		Person *p = new Person[N];
+		int num;
+		std::vector<Person> vp(N);
 		for (int i = 0; i < N; i++)
 		{
-			std::cin >> p[i];
+			std::cin >>num >> vp[i];
 		}
-		
+		sort(vp.begin(), vp.end(), cmp);//先按体重排个序
+
+		std::vector<int> dp(N);//dp与对应vp对应的位置代表前面到当前位置罗汉塔的最大高度
+		dp[0] = 1;//初始化第一个元素为1
+		for (int i = 1; i < N; i++)
+		{
+			dp[i] = 1;//每次到达某个元素时塔最低高度为1
+			for (int j = 0; j < i; j++)
+			{
+				//每次都从第一个元素找起，找到当前位置中使罗汉塔高度最高
+				//若上一个元素vp[j]比当前元素vp[i]升高第高(从上往下算)，
+				//且上一个元素所记录的最高高度+1(dp[j]+1)要大于当前元素的高度dp[i]
+				if (vp[j].height <= vp[i].height && dp[j] + 1 > dp[i])
+				{
+					//如果满足，就跟新当前元素的最高高度
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		int Max = dp[0];
+		for (int i = 1; i < N; i++)//找出高度记录表中最大高度
+		{
+			Max = Max > dp[i] ? Max : dp[i];
+		}
+
+		std::cout << Max << std::endl;
 	}
-    return 0;
+	return 0;
 }
--- C++_CN_Day56/C++_CN_Day56/马戏团.cpp
+++ C++_CN_Day56/C++_CN_Day56/马戏团.cpp
@@ -1,25 +1,123 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
 
-int main1()
+bool cmp1(pair<int, int> m1, pair<int, int> m2)
+{
+	return m1.first >= m2.first;
+}
+bool cmp2(pair<int, int> m1, pair<int, int> m2)
+{
+	return m1.first <= m2.first;
+}
+
+int main()
 {
 	int n;
-	while (std::cin >> n)
+	while (cin >> n)
 	{
-		std::vector<int> v(n);
-		int k, d, Max, mul;
+		unordered_map<int, int> m1;//存取正数(包括0);
+		unordered_map<int, int> m2;//存取负数;
+		int k, d, tmp, Max, mul;
 		for (int i = 0; i < n; i++)
 		{
-			std::cin >> v[i];
+			cin >> tmp;
+			if (tmp >= 0)
+			{
+				m1[tmp] = i;
+			}
+			else
+			{
+				m2[tmp] = i;
+			}
 		}
-		std::cin >> k >> d;
+		cin >> k >> d;
 
-		for (int i = 1; i < n; i++)
+		sort(m1.begin(), m1.end(),cmp1);
+		sort(m1.begin(), m1.end(),cmp2);
+		if (k == 0)
+		{
+			cout << 0 << endl;
+		}
+		else if(k == 1)//乘数为偶数，可以由正负数交叉完成
+		{
+			cout << m1.begin()->first << endl;
+		}
+		else
 		{
 
 		}
-		std::cout << max
 	}
     return 0;
+}
+
+
+链接：https://www.nowcoder.com/questionTerminal/661c49118ca241909add3a11c96408c8
+来源：牛客网
+
+#include <stdio.h>
+inline long long max(long long a, long long b){ return (a>b ? a : b); }
+inline long long min(long long a, long long b){ return (a>b ? b : a); }
+
+int main(){
+	int N, K, D, i, j, k;
+	long long stu[51], fm[11][51], fn[11][51], ans;
+	while (~scanf("%d", &N)){
+		for (i = 0; i<N; scanf("%lld", &stu[++i]));
+		scanf("%d %d", &K, &D);
+		for (i = 0; i <= K; ++i)
+		for (j = 0; j <= N; fm[i][j] = fn[i][j] = 0, ++j);
+		for (i = 1, ans = ~0LL; i <= N; ++i){
+			fm[1][i] = fn[1][i] = stu[i];
+			for (k = 2; k <= K; ++k){
+				for (j = i - 1; j>0 && i - j <= D; --j){
+					fm[k][i] = max(fm[k][i], max(fm[k - 1][j] * stu[i], fn[k - 1][j] * stu[i]));
+					fn[k][i] = min(fn[k][i], min(fn[k - 1][j] * stu[i], fm[k - 1][j] * stu[i]));
+				}
+			}
+			ans = max(ans, fm[K][i]);
+		}
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n;
+	while (cin >> n){
+		vector<int> a(n);
+		for (int i = 0; i < n; i++){
+			cin >> a[i];
+		}
+		int k, d;
+		cin >> k >> d;
+		vector<vector<long long>> dp_max(n, vector<long long>(k + 1, 0));
+		vector<vector<long long>> dp_min(n, vector<long long>(k + 1, 0));
+		for (int i = 0; i < n; i++){
+			dp_max[i][1] = a[i];
+			dp_min[i][1] = a[i];
+		}
+		for (int i = 0; i < n; i++){
+			for (int j = 2; j <= k; j++){
+				for (int m = max(0, i - d); m <= i - 1; m++){
+					dp_max[i][j] = max(dp_max[i][j], max(dp_max[m][j - 1] * a[i], dp_min[m][j - 1] * a[i]));
+					dp_min[i][j] = min(dp_min[i][j], min(dp_min[m][j - 1] * a[i], dp_max[m][j - 1] * a[i]));
+				}
+			}
+		}
+		long long max_value = dp_max[k - 1][k];
+		for (int i = k; i < n; i++){
+			max_value = max(max_value, dp_max[i][k]);
+		}
+		cout << max_value << endl;
+	}
+	return 0;
 }
