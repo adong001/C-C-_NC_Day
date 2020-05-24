5e7ff9b95a4e2db4da237228c2d64c1d94cefa53
adong <1792095378@qq.com>
2020-05-24 22:00:06 +0800

    123

---
+++ 秋招冲刺刷题0524/秋招冲刺刷题0524/3.无重复字符的最长子串.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<map>
+#include<set>
+using namespace std;
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+//
+//示例 1 :
+//输入 : "abcabcbb"
+//输出 : 3
+//解释 : 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+//
+//示例 2 :
+//输入 : "bbbbb"
+//输出 : 1
+//解释 : 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+//
+//示例 3 :
+//输入 : "pwwkew"
+//输出 : 3
+//解释 : 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+class Solution 
+{
+public:
+	int lengthOfLongestSubstring(string s) 
+	{
+		int max = 0, count;
+		int Size = s.size();
+		map<char, int> mc;
+		for (int start = 0, end = 0; end < Size; end = start)
+		{
+			count = 0;
+			mc.clear();
+			bool flag = false;
+			for (; end < Size; end++, count++)
+			{
+				if (mc.count(s[end]))
+				{
+					flag = true;
+					break;
+				}
+				mc[s[end]] = end;
+			}
+			start = flag ? mc[s[end]] + 1 : end;
+			max = max > count ? max : count;
+		}
+		return max;
+	}
+};
+
+int lengthOfLongestSubstring(string s)
+{
+	int l = 0, r = l, _max = 0;
+	set<char> sc;
+	while (r != s.size())
+	{
+		if (sc.find(s[r]) == sc.end())//没找到
+		{
+			sc.insert(s[r++]);
+			_max = _max > r - l ?_max :r - 1;
+		}
+		else sc.erase(s[l++]);
+	}
+	return _max;
+}
+int main1()
+{
+	Solution s;
+	cout << s.lengthOfLongestSubstring("dvdf") << endl;
+    return 0;
+}
---
+++ 秋招冲刺刷题0524/秋招冲刺刷题0524/最长回文子串.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+//
+//示例 1：
+//输入 : "babad"
+//输出 : "bab"
+//注意 : "aba" 也是一个有效答案。
+//
+//示例 2：
+//输入 : "cbbd"
+//输出 : "bb"
+
+class Solution
+{
+public:
+	bool IsPalindrom(string& s)
+	{
+		int end = s.size() - 1;
+		for (int start = 0; start <= end; start++, end--)
+		{
+			if (s[start] != s[end])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	string longestPalindrome(string s)
+	{
+		int max = 0;
+
+	}
+};
+int main()
+{
+    return 0;
+}
