1fff593f60781121f139b299f57463628c25b024
adong <1792095378@qq.com>
2020-05-24 21:05:18 +0800

    23

--- 秋招冲刺刷题0519/秋招冲刺刷题0519/设计一个线程安全的hash.cpp
+++ 秋招冲刺刷题0519/秋招冲刺刷题0519/设计一个线程安全的hash.cpp
@@ -1,8 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<mutex>
+#include<map>
+#include<string>
 using namespace std;
 
-int main3()
+map<string, string> g_m;
+mutex mu;
+
+void Set(const string& key, const string& value)//设置key-value
+{
+	mu.lock();
+	g_m[key] = value;
+	mu.unlock();
+}
+
+string Get(const string& key)//根据key查询value，如果key不存在，返货字符none
+{
+	mu.lock();
+	if (g_m.count(key))
+	{
+		mu.unlock();
+		return g_m[key];
+	}
+	mu.unlock();
+	return "none";
+}
+
+int main()
 {
     return 0;
 }
